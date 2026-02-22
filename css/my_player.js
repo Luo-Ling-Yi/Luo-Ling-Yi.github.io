3d7eb42f45655b0c144bdf2af2da9478d1a03c8f
Luo-Ling-Yi <3439436909@qq.com>
2026-02-22 22:52:05 +0800

    Site updated: 2026-02-22 22:52:04

---
+++ css/my_player.js
@@ -0,0 +1,20 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    mini: false,
+    listFolded: true,
+    audio: [
+        { name: 'Himawari', artist: '乐正绫', url: '/songs/【乐正绫】Himawari.mp3', cover: '/img/cover.jpg' },
+        { name: '隙光海', artist: '永夜minus', url: '/songs/【永夜minus】隙光海.mp3', cover: '/img/cover.jpg' },
+        { name: '风衣', artist: '永夜minus', url: '/songs/【永夜minus】风衣.mp3', cover: '/img/cover.jpg' },
+        { name: '不年轻人', artist: '星尘infinity', url: '/songs/【星尘infinity】不年轻人.mp3', cover: '/img/cover.jpg' },
+        { name: '致你', artist: '南北组', url: '/songs/【南北组】致你.mp3', cover: '/img/cover.jpg' },
+        { name: '相思若循', artist: '南北组', url: '/songs/【南北组】相思若循.mp3', cover: '/img/cover.jpg' },
+        { name: '桔梗花', artist: '乐正绫', url: '/songs/【乐正绫】桔梗花.mp3', cover: '/img/cover.jpg' },
+        { name: '芭蕉夜雨', artist: '乐正绫', url: '/songs/【乐正绫】芭蕉夜雨.mp3', cover: '/img/cover.jpg' },
+        { name: '到此为止', artist: '星尘infinity', url: '/songs/【星尘infinity】到此为止.mp3', cover: '/img/cover.jpg' },
+        { name: '星星与玻璃', artist: '赤羽', url: '/songs/【赤羽】星星与玻璃.mp3', cover: '/img/cover.jpg' },
+        { name: '碎镜', artist: '赤羽', url: '/songs/【赤羽】碎镜.mp3', cover: '/img/cover.jpg' }
+	{name:"无雪的故乡",artist:"乐正绫",url:"/songs/【乐正绫】无雪的故乡.mp3",cover:"/img/cover.jpg"}
+    ]
+});
